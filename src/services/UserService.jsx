7a9c53acd5ffdeb4f1c639110833694e9493d24f
Felipe <fe.ortegah@duocuc.cl>
2025-11-20 22:30:07 -0300

    intetno 5.2 UserService

--- src/services/UserService.jsx
+++ src/services/UserService.jsx
@@ -1,18 +1,27 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://backend-fullstackv1.onrender.com/api/usuarios';
+
 class UserService {
     login(usuario) {
-        return axios.post(`${BASE_URL}/usuarios/login`, usuario);
+        return axios.post(`${BASE_URL}/login`, usuario);
     }
+
     createUser(usuario) {
-        return axios.post(`${BASE_URL}/usuarios`, usuario);
+        return axios.post(`${BASE_URL}`, usuario);
     }
+
     verifyToken(token) {
-        return axios.get(`${BASE_URL}/usuarios/verify`, {
+        return axios.get(`${BASE_URL}/verify`, {
             headers: { Authorization: `Bearer ${token}` }
         });
     }
+
     logout(token) {
-        return axios.post(`${BASE_URL}/usuarios/logout`, {}, {
+        return axios.post(`${BASE_URL}/logout`, {}, {
             headers: { Authorization: `Bearer ${token}` }
         });
     }
 }
+
+export default new UserService();
