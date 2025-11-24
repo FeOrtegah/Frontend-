f94f1469d1434c341bb9a9520cfe349f192e6850
Esteban Gamboa <61128292+ealejandro23@users.noreply.github.com>
2025-11-24 00:17:50 -0300

    fsfsdfsdf

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { ProductProvider } from './context/ProductContext' // ← Importar el Provider
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,18 +15,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </React.StrictMode>,
 )
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./styles/global.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
->>>>>>> 117f769bce0402b3d4c3cc178c29a8febc55edc9
