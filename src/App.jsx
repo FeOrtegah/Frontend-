33d699b5ec8584d6ac862902e101f0d38f2abedf
FeOrtegah <fe.ortegah@duocuc.cl>
2025-11-24 20:53:59 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,137 +1,52 @@
-import React from 'react';
-import { Routes, Route, useLocation, Navigate } from "react-router-dom";
-import Navbar from "./components/organisms/Navbar";
-import Footer from "./components/organisms/Footer";
-import Home from "./pages/user/Home";
-import Carrito from "./pages/user/Carrito";
-import RopaHombre from "./pages/user/RopaHombre";
-import RopaMujer from "./pages/user/RopaMujer";
-import RopaInfantil from "./pages/user/RopaInfantil";
-import ProductDetail from "./pages/user/ProductDetail";
-import Pago from "./pages/user/Pago";
-import Confirmacion from "./pages/user/Confirmacion";
-import Registro from "./pages/user/Registro";
-import Login from "./pages/user/Login";
-import MiCuenta from "./pages/user/MiCuenta";
-import Blogs from "./pages/user/Blogs";
-import Ayuda from './pages/user/Ayuda.jsx';
-import Noticias from "./pages/user/Noticias";
-import HomeAdmin from "./pages/admin/HomeAdmin.jsx";
-import { useProducts } from './context/ProductContext';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import LoginPage from "./pages/LoginPage";
+import Home from "./pages/Home";
+import Productos from "./pages/Productos";
+import Perfil from "./pages/Perfil";
 
 function App() {
-  const [carrito, setCarrito] = React.useState([]);
-  const [user, setUser] = React.useState(null);
-  const location = useLocation();
-
-  const { products, loading, error } = useProducts();
-
-  React.useEffect(() => {
-    const savedCarrito = JSON.parse(localStorage.getItem("carrito")) || [];
-    setCarrito(savedCarrito);
-
-    const savedUser = JSON.parse(sessionStorage.getItem("usuarioActivo"));
-    if (savedUser) {
-      setUser(savedUser);
-    }
-  }, []);
-
-  React.useEffect(() => {
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-  }, [carrito]);
-
-  const hideNavbarFooterPaths = ["/registro", "/admin", "/login"];
-  const shouldHideNavbarFooter = hideNavbarFooterPaths.includes(location.pathname);
-  
-  const isAuthenticated = () => {
-    return user !== null;
-  };
-  
-  const isAdmin = () => {
-    return user && (user.rol === 'admin' || user.rol?.nombreRol === 'admin');
-  };
-
   return (
-    <div className="d-flex flex-column min-vh-100">
-
-      {/* Ocultar Navbar */}
-      {!shouldHideNavbarFooter && (
-        <Navbar carrito={carrito} user={user} setUser={setUser} />
-      )}
-
-      <main className="flex-grow-1">
+    <AuthProvider>
+      <Router>
         <Routes>
-          {/* Rutas públicas */}
-          <Route path="/" element={<Home />} />
-          <Route path="/hombre" element={<RopaHombre />} />
-          <Route path="/hombre/:subcategoria" element={<RopaHombre />} />
-          <Route path="/mujer" element={<RopaMujer />} />
-          <Route path="/mujer/:subcategoria" element={<RopaMujer />} />
-          <Route path="/infantil" element={<RopaInfantil />} />
-          <Route path="/infantil/:subcategoria" element={<RopaInfantil />} />
-          
-          <Route 
-            path="/producto/:id" 
-            element={<ProductDetail carrito={carrito} setCarrito={setCarrito} />} 
-          />
-          
-          <Route path="/blog" element={<Blogs />} />
-          <Route path="/ayuda" element={<Ayuda />} />
-          <Route path="/noticias" element={<Noticias />} />
-          <Route path="/registro" element={<Registro setUser={setUser} />} />
-          <Route path="/login" element={<Login setUser={setUser} />} />
 
-          {/* Rutas que requieren autenticación */}
-          <Route 
-            path="/carrito" 
+          {/* Login */}
+          <Route path="/login" element={<LoginPage />} />
+
+          {/* Rutas protegidas */}
+          <Route
+            path="/"
             element={
-              isAuthenticated() ? 
-                <Carrito carrito={carrito} setCarrito={setCarrito} /> : 
-                <Navigate to="/registro" replace state={{ from: location }} />
-            } 
-          />
-          
-          <Route 
-            path="/pago" 
-            element={
-              isAuthenticated() && carrito.length > 0 ? 
-                <Pago carrito={carrito} setCarrito={setCarrito} /> : 
-                carrito.length === 0 ? 
-                  <Navigate to="/carrito" replace /> : 
-                  <Navigate to="/registro" replace state={{ from: location }} />
-            } 
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
           />
 
-          {/* Ruta de confirmación */}
-          <Route path="/confirmacion" element={<Confirmacion />} />
-          
-          <Route 
-            path="/micuenta" 
+          <Route
+            path="/productos"
             element={
-              isAuthenticated() ? 
-                <MiCuenta user={user} setUser={setUser} /> : 
-                <Navigate to="/registro" replace state={{ from: location }} />
-            } 
+              <ProtectedRoute>
+                <Productos />
+              </ProtectedRoute>
+            }
           />
 
-          {/* Ruta de admin */}
-          <Route 
-            path="/admin" 
+          <Route
+            path="/perfil"
             element={
-              isAdmin() ? 
-                <HomeAdmin /> : 
-                <Navigate to="/" replace />
-            } 
+              <ProtectedRoute>
+                <Perfil />
+              </ProtectedRoute>
+            }
           />
-          
-          {/* Ruta 404 - redirigir al home */}
-          <Route path="*" element={<Navigate to="/" replace />} />
+
         </Routes>
-      </main>
-
-      {/* Ocultar footer*/}
-      {!shouldHideNavbarFooter && <Footer />}
-    </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
