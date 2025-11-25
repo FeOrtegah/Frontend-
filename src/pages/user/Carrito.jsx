d52ddee164eadf0600ca7e9fc1ebe53f47c1acb8
Esteban Gamboa <61128292+ealejandro23@users.noreply.github.com>
2025-11-24 22:07:51 -0300

    Update Carrito.jsx

--- src/pages/user/Carrito.jsx
+++ src/pages/user/Carrito.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Container, Row, Col, Card, Button, Table, Alert, Spinner } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import OrderService from "/src/services/OrderService";
-import "./styles/global.css";
 
 const Carrito = ({ carrito, setCarrito }) => {
   const navigate = useNavigate();
