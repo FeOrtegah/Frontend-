008b011c24c067eaaa9f69f2d14acb0564e329e3
Esteban Gamboa <61128292+ealejandro23@users.noreply.github.com>
2025-11-24 21:55:00 -0300

    Change import statement for global.css

--- src/pages/user/RopaMujer.jsx
+++ src/pages/user/RopaMujer.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useMemo } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useProducts } from '../../context/ProductContext';
-import './global.css';
+import "../../styles/global.css";
 
 const RopaMujer = () => {
   const { subcategoria } = useParams();
