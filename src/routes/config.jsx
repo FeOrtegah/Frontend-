cff08331855145343a9229998b64c0f81207c955
Esteban Gamboa <61128292+ealejandro23@users.noreply.github.com>
2025-11-25 19:02:22 -0300

    Update config.jsx

--- src/routes/config.jsx
+++ src/routes/config.jsx
@@ -1,4 +1,4 @@
-// routes/config.js
+
 import { lazy } from "react";
 
 const Home = lazy(() => import("../pages/user/Home"));
@@ -17,22 +17,25 @@ const HomeAdmin = lazy(() => import("../pages/admin/HomeAdmin"));
 const Auth = lazy(() => import("../pages/auth/Auth"));
 
 export const appRoutes = [
-  { path: "/", element: <Home /> },
-  { path: "/hombre", element: <RopaHombre /> },
-  { path: "/hombre/:subcategoria", element: <RopaHombre /> },
-  { path: "/mujer", element: <RopaMujer /> },
-  { path: "/mujer/:subcategoria", element: <RopaMujer /> },
-  { path: "/infantil", element: <RopaInfantil /> },
-  { path: "/infantil/:subcategoria", element: <RopaInfantil /> },
-  { path: "/producto/:id", element: <ProductDetail /> },
-  { path: "/blog", element: <Blogs /> },
-  { path: "/ayuda", element: <Ayuda /> },
-  { path: "/noticias", element: <Noticias /> },
-  { path: "/auth", element: <Auth /> },
-  { path: "/carrito", element: <Carrito /> },
+  { path: "/", element: <Home />, public: true },
+  { path: "/hombre", element: <RopaHombre />, public: true },
+  { path: "/hombre/:subcategoria", element: <RopaHombre />, public: true },
+  { path: "/mujer", element: <RopaMujer />, public: true },
+  { path: "/mujer/:subcategoria", element: <RopaMujer />, public: true },
+  { path: "/infantil", element: <RopaInfantil />, public: true },
+  { path: "/infantil/:subcategoria", element: <RopaInfantil />, public: true },
+  { path: "/producto/:id", element: <ProductDetail />, public: true },
+  { path: "/blog", element: <Blogs />, public: true },
+  { path: "/ayuda", element: <Ayuda />, public: true },
+  { path: "/noticias", element: <Noticias />, public: true },
+  { path: "/auth", element: <Auth />, public: true, onlyPublic: true },
+  { path: "/carrito", element: <Carrito />, private: true },
   { path: "/pago", element: <Pago />, private: true },
   { path: "/confirmacion", element: <Confirmacion />, private: true },
   { path: "/micuenta", element: <MiCuenta />, private: true },
-  { path: "/admin", element: <HomeAdmin />, isAdmin: true },
-  { path: "*", element: <div className="text-center text-3xl py-10">404 - Página no encontrada</div> },
+  
+  { path: "/admin", element: <HomeAdmin />, private: true, admin: true },
+  { path: "/admin/*", element: <HomeAdmin />, private: true, admin: true },
+
+  { path: "*", element: <div className="text-center text-3xl py-10">404 - Página no encontrada</div>, public: true },
 ];
