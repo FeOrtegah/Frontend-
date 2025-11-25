eb81c42825961b806b155a59d0e2b01fde2dc9d8
Esteban Gamboa <61128292+ealejandro23@users.noreply.github.com>
2025-11-24 21:53:58 -0300

    Update Pago.jsx

--- src/pages/user/Pago.jsx
+++ src/pages/user/Pago.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import VentaService from '../../services/VentaService';
-import './global.css';
+import "../../styles/global.css";
 
 const Pago = ({ carrito, setCarrito }) => {
     const [formData, setFormData] = useState({
