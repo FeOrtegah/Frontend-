40f3e1663b842da5a9897afec6cde4763c18ef98
FeOrtegah <fe.ortegah@duocuc.cl>
2025-11-25 21:44:58 -0300

    Update HomeAdmin.jsx

--- src/pages/admin/HomeAdmin.jsx
+++ src/pages/admin/HomeAdmin.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Card, Table, Badge, Button, Modal, Form, Spinner, Alert } from 'react-bootstrap';
 import ProductService from '../../services/ProductService';
-import homeData from '../../Facciones/data/homeData';
+import homeData from '../../admin/homeData';
 
 const ProductModal = ({ show, handleClose, handleSubmit, loading }) => {
   const [formData, setFormData] = useState({
