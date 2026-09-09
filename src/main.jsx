66bcf3c392dae316e2523a5a1f899fe9e318c84b
TheAntonio <ant.cornejom@duocuc.cl>
2026-09-08 21:41:07 -0300

    Add global CSS styles import to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { ProductProvider } from './context/ProductContext' // ← Importar el Provider
 import './index.css'
+import './styles/global.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
